716783564dba1308da7dd3d9807420a5398586f2
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-02-25 10:11:03 -0800

    Add third party apis

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
+  L.mapquest.key= 'mJ9o8HJ9ev64aQyKRKK59kRZK4VnY6sS';
+  var map =  L.mapquest.map('map', {
+    center: [32.88,-117.236],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
 	// add your code here
+  L.marker([32.88,-117.236]).addTo(map);
 }
